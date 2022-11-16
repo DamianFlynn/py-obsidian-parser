f99b548678eb3da86bf0b1ba1d86d6b72683d115
Damian Flynn <info@damianflynn.com>
2022-11-16 14:33:01 +0000

    feat: create a class
    Multiply two numbers

--- script.py
+++ script.py
@@ -1,7 +1,14 @@
-def multiply(number: int, multiplier: int):
+class Multiplication:
     """Multiply number by multiplier."""
-    return number * multiplier
-    
+    def __init__(self, multiplier: int):
+        """Initialize multiplier."""
+        self.multiplier = multiplier
+
+    def multiply(self, number: int) -> int:
+        """Multiply number by multiplier."""
+        return number * self.multiplier
+
 
 # Display the result
-print(multiply(5, 2))
+multipication = Multiplication(5)
+print(multipication.multiply(2))
