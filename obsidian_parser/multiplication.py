536f4261775033a6d8da7bdb74d91b6604154fb4
Damian Flynn <info@damianflynn.com>
2022-11-16 23:17:36 +0000

    feat: class doc string
    doc strings

--- obsidian_parser/multiplication.py
+++ obsidian_parser/multiplication.py
@@ -1,10 +1,26 @@
 class Multiplication:
-    """Multiply number by multiplier."""
+    """
+    Instantiate a multiplication operation.
+
+    Numbers will be multiplied by the given multiplier.
+    
+    :param multiplier: The multiplier.
+    :type multiplier: float
+    """
+
     def __init__(self, multiplier: float):
         """Initialize multiplier."""
         self.multiplier = multiplier
 
     def multiply(self, number: float) -> float:
-        """Multiply number by multiplier."""
+        """
+        Multiply a given number by the multiplier.
+        
+        :param number: The number to multiply.
+        :type number: float
+    
+        :return: The result of the multiplication.
+        :rtype: float
+        """
         return number * self.multiplier
 
